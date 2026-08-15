836474520901a2ddec878914ddae5001da54569c
AlmightySatan <43804777+Almighty-Satan@users.noreply.github.com>
2026-08-15 17:00:48 +0200

    Move BombermanMap to its own file

---
+++ src/main/java/de/varoplugin/bomberman/config/BombermanMap.java
@@ -0,0 +1,90 @@
+package de.varoplugin.bomberman.config;
+
+import io.github.almightysatan.jaskl.InvalidTypeException;
+import io.github.almightysatan.jaskl.ObjectMapper;
+import io.github.almightysatan.jaskl.Type;
+import io.github.almightysatan.jaskl.ValidationException;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Unmodifiable;
+import org.jspecify.annotations.NonNull;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
+
+public class BombermanMap {
+
+    public final String name;
+    public final List<Location> spawns;
+    public final Location corner0, corner1;
+
+    public BombermanMap(String name, List<Location> spawns, Location corner0, Location corner1) {
+        this.name = name;
+        this.spawns = spawns;
+        this.corner0 = corner0;
+        this.corner1 = corner1;
+    }
+
+    static ObjectMapper<BombermanMap> getMapper() {
+        ObjectMapper<Location> locationMapper = new ObjectMapper<Location>() {
+            @Override
+            public @NonNull Location createInstance(@Unmodifiable @NotNull Map<@NotNull String, @NotNull Object> values) throws InvalidTypeException, ValidationException {
+                return new Location(Bukkit.getWorld((String) values.get("world")), ((BigDecimal) values.get("x")).doubleValue(),
+                        ((BigDecimal) values.get("y")).doubleValue(), ((BigDecimal) values.get("z")).doubleValue(),
+                        ((BigDecimal) values.get("yaw")).floatValue(), ((BigDecimal) values.get("pitch")).floatValue());
+            }
+
+            @Override
+            public @NotNull @Unmodifiable Map<@NotNull String, @NotNull Object> readValues(@NonNull Location instance) throws InvalidTypeException {
+                return Map.of("world", instance.getWorld().getName(), "x", instance.getX(), "y", instance.getY(), "z", instance.getZ(), "yaw", instance.getYaw(), "pitch", instance.getPitch());
+            }
+
+            @Override
+            public @NotNull Class<Location> getObjectClass() {
+                return Location.class;
+            }
+
+            @Override
+            public @NotNull Property<?> @NotNull [] getProperties() {
+                return new Property[] {
+                        Property.of("world", Type.STRING),
+                        Property.of("x", Type.BIG_DECIMAL),
+                        Property.of("y", Type.BIG_DECIMAL),
+                        Property.of("z", Type.BIG_DECIMAL),
+                        Property.of("yaw", Type.BIG_DECIMAL),
+                        Property.of("pitch", Type.BIG_DECIMAL)
+                };
+            }
+        };
+
+        return new ObjectMapper<BombermanMap>() {
+            @SuppressWarnings("unchecked")
+            @Override
+            public @NonNull BombermanMap createInstance(@Unmodifiable @NotNull Map<@NotNull String, @NotNull Object> values) throws InvalidTypeException, ValidationException {
+                return new BombermanMap((String) values.get("name"), (List<Location>) values.get("spawns"), (Location) values.get("corner_0"), (Location) values.get("corner_1"));
+            }
+
+            @Override
+            public @Unmodifiable @NotNull Map<@NotNull String, @NotNull Object> readValues(@NonNull BombermanMap instance) throws InvalidTypeException {
+                return Map.of("name", instance.name, "spawns", instance.spawns, "corner_0", instance.corner0, "corner_1", instance.corner1);
+            }
+
+            @Override
+            public @NotNull Class<BombermanMap> getObjectClass() {
+                return BombermanMap.class;
+            }
+
+            @Override
+            public @NotNull Property<?> @NotNull [] getProperties() {
+                return new Property[] {
+                        Property.of("name", Type.STRING),
+                        Property.of("spawns", Type.list(Type.custom(locationMapper))),
+                        Property.of("corner_0", Type.custom(locationMapper)),
+                        Property.of("corner_1", Type.custom(locationMapper)),
+                };
+            }
+        };
+    }
+}
