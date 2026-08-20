941bb9cd5a2e689703c487f74427f39a6ff76d87
CuukyOfficial <just.cookie.jc@gmail.com>
2026-08-20 18:06:24 +0200

    feat: item vote hook

--- src/main/java/de/varoplugin/bomberman/game/lobby/PlayerLobbyStateJob.java
+++ src/main/java/de/varoplugin/bomberman/game/lobby/PlayerLobbyStateJob.java
@@ -2,11 +2,23 @@ package de.varoplugin.bomberman.game.lobby;
 
 import de.varoplugin.bomberman.Bomberman;
 import de.varoplugin.bomberman.game.AbstractStatePlayerJob;
+import de.varoplugin.cfw.item.ItemBuilder;
+import de.varoplugin.cfw.player.hook.item.HookItemInteractEvent;
+import de.varoplugin.cfw.player.hook.item.ItemHook;
+import de.varoplugin.cfw.player.hook.item.PlayerItemHookBuilder;
 import org.bukkit.GameMode;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class PlayerLobbyStateJob extends AbstractStatePlayerJob {
+
+    private final Map<Player, List<ItemHook>> hooks = new HashMap<>();
 
     protected PlayerLobbyStateJob(Bomberman plugin) {
         super(plugin);
@@ -17,11 +29,17 @@ public class PlayerLobbyStateJob extends AbstractStatePlayerJob {
         player.setExp(0);
         player.setLevel(0);
         player.setGameMode(GameMode.ADVENTURE);
-        player.getInventory().clear();
-        player.getInventory().setArmorContents(new ItemStack[4]);
+
+        ItemHook hook = new PlayerItemHookBuilder().slot(1)
+                .item(ItemBuilder.itemStack(new ItemStack(Material.PAPER)).displayName("§aVote!").build())
+                .subscribe(HookItemInteractEvent.class, _ ->
+                        player.sendMessage("Du hast gevotet!")).complete(player, this.plugin);
+        hooks.putIfAbsent(player, new ArrayList<>());
+        hooks.get(player).add(hook);
     }
 
     @Override
     public void disable(Player player) {
+        hooks.getOrDefault(player, List.of()).forEach(ItemHook::unregister);
     }
 }
