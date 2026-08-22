3d40b98228c6a9dae8e3cfc75a038087ad380254
CuukyOfficial <just.cookie.jc@gmail.com>
2026-08-22 13:49:22 +0200

    fix: player spec on non game start

--- src/main/java/de/varoplugin/bomberman/game/running/PlayerGameStateJob.java
+++ src/main/java/de/varoplugin/bomberman/game/running/PlayerGameStateJob.java
@@ -3,7 +3,6 @@ package de.varoplugin.bomberman.game.running;
 import de.varoplugin.bomberman.game.AbstractStatePlayerJob;
 import de.varoplugin.bomberman.model.BombPlayer;
 import de.varoplugin.cfw.item.ItemBuilder;
-import de.varoplugin.cfw.player.hook.item.HookItemInteractEvent;
 import de.varoplugin.cfw.player.hook.item.PlayerItemHookBuilder;
 import de.varoplugin.cfw.player.hud.NameTagGroup;
 import org.bukkit.GameMode;
@@ -15,31 +14,43 @@ import org.bukkit.potion.PotionEffectType;
 
 public class PlayerGameStateJob extends AbstractStatePlayerJob {
 
-    private final NameTagGroup nameTagGroup = new NameTagGroup();
+    private final RunningHeartbeat heartbeat;
+    private final NameTagGroup nameTagGroup;
 
     protected PlayerGameStateJob(RunningHeartbeat heartbeat) {
         super(heartbeat.getPlugin());
+
+        this.heartbeat = heartbeat;
+        this.nameTagGroup = new NameTagGroup();
     }
 
     @Override
     public void enable(Player player) {
+        BombPlayer bPlayer = this.plugin.getPlayer(player);
+        if (this.heartbeat.getCountdown() != 600) {
+            bPlayer.enableSpectator(this.plugin);
+            return;
+        }
+
         player.setExp(0);
         player.setLevel(0);
         player.setGameMode(GameMode.SURVIVAL);
         player.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, PotionEffect.INFINITE_DURATION, 255, false, false, false));
 
-        BombPlayer bPlayer = this.plugin.getPlayer(player);
         this.nameTagGroup.register(bPlayer.getScoreboardInstance(), false, "" ,"");
 
         for (int i = 0; i < player.getInventory().getSize(); i++) {
             new PlayerItemHookBuilder().slot(i)
                     .item(ItemBuilder.itemStack(new ItemStack(Material.TNT)).displayName("§cSprengstoff").build())
-                    .subscribe(HookItemInteractEvent.class, _ ->
-                            player.sendMessage("Du hast gevotet!")).complete(player, this.plugin);
+                    .movable(false)
+                    .droppable(false)
+                    .cancel(false).complete(player, this.plugin);
         }
     }
 
     @Override
     public void disable(Player player) {
+        player.getInventory().clear();
+        this.nameTagGroup.unRegister(player);
     }
 }
